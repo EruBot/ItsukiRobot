c739fac7d89cc5f11c802242b8cab86c3db3e963
EruBot <64939964+EruBot@users.noreply.github.com>
2021-06-25 09:38:57 +0700

    Update handlers.py

--- SaitamaRobot/modules/helper_funcs/handlers.py
+++ SaitamaRobot/modules/helper_funcs/handlers.py
@@ -4,6 +4,7 @@ from SaitamaRobot import (DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES)
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
+from time import sleep
 from pyrate_limiter import (BucketFullException, Duration, RequestRate, Limiter,
                             MemoryListBucket)
 
